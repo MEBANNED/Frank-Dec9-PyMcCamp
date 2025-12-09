1c5b8b14d8fcfe113f3b2fb3191372496ffa4e57
MEBANNED <frankchencty@gmail.com>
2025-12-09 06:53:48 +0000

    Dec 9, 2025, 6:53 AM

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -9,10 +9,9 @@ print("hello from day1")
 
 ########################################################################
 # Task 2:
-print("My name is Frank Chen")
-print("I am nine years old")
-print("I attend townsville primary school")
-print("if I had $2,000 I")
+
+
+
 ########################################################################
 # Task 3:
 
