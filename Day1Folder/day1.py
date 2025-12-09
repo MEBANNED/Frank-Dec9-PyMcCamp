c9ce46637ae7175778ef703282fbfa97d4a00864
MEBANNED <frankchencty@gmail.com>
2025-12-09 07:17:40 +0000

    Dec 9, 2025, 7:17 AM

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -34,7 +34,7 @@ print('My name is ' + name + ", I'm gay")
 def selfIntro():
     print("My name is Frank")
     print("I am 9 years old")
-    print()
+    print(I attend )
 ########################################################################
 # Task 7:
 
