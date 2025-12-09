e6440b63420039be4904561cd409d31103e46f63
MEBANNED <frankchencty@gmail.com>
2025-12-09 06:49:35 +0000

    Dec 9, 2025, 6:49 AM

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -20,6 +20,7 @@ animal2 = "Trallero Tralala"
 animal3 = "Tripi Tropi"
 print(animal1)
 print(animal2)
+print()
 ########################################################################
 # Task 4:
 
