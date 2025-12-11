d0e34fe01bd0d144ad2cbacdcac6626ba9b40899
MEBANNED <frankchencty@gmail.com>
2025-12-11 06:35:51 +0000

    Dec 11, 2025, 6:35 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -7,7 +7,7 @@ print("hello from day3")
 title = input("What is your title")
 name = input("What's ur name")
 command = input("What's your command")
-print(title + name + "commands " + command )
+print(title + " " +name + " commands " + command )
 ########################################################################
 # Task 2:
 
